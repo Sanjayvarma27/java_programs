7b7e2996d7966c9a1857953a48599f68ff04f8e6
User <User@hp>
2025-10-04 16:30:34 +0530

    This is the count program

---
+++ Basic_programs/src/pack1/Count_Numbers.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class Count_Numbers {
+
+	public static void main(String args[]) {
+	
+		System.out.println("count the number");
+		
+		int num= 123;
+		int count=0;
+		
+		while(num>0) {
+		//num=num+	num%10;
+		num= num/10;
+		count++;
+		}
+		System.out.println(count);
+}
+}
