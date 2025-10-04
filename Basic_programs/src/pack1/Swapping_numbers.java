e022c01e8c3cde0aac8262b4028bc0d38399c3fe
User <User@hp>
2025-10-04 15:13:57 +0530

    i added swapping of number program

---
+++ Basic_programs/src/pack1/Swapping_numbers.java
@@ -0,0 +1,20 @@
+package pack1;
+
+public class Swapping_numbers {
+
+	public static void main(String args[]) {
+
+	int a =10;
+	int b=20;
+	int c;
+	System.out.println("before swapping" +a + b);
+	
+	c=a;
+	a=b;
+	b=c;
+	System.out.println("After swapping"+ a + b);
+	
+	
+	
+	}
+}
