88befb0f93a9ed45c938ce14ce676c086e35b824
User <User@hp>
2025-10-05 19:20:11 +0530

    String how many value present it

---
+++ Basic_programs/src/pack1/Count_charcters.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class Count_charcters {
+
+	public static void main(String args[]) {
+		
+		String s = "sanjayvarma";
+		
+	Integer a= 	s.length();
+	/*
+	 * int value = 0; for(int i=0;i<a.SIZE-1;i++) {
+	 * 
+	 * value=value+ a.SIZE; }
+	 */
+System.out.println(a);
+	}
+}
