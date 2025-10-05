3ad34e1e33f8c641f05004aff383594fde13c432
User <User@hp>
2025-10-05 17:32:02 +0530

    this is the missing number program in arrays

---
+++ Basic_programs/src/pack1/Find_missing_number_in_array.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class Find_missing_number_in_array {
+
+	public static void main(String[] args) {
+		
+	
+	int a []= {1,2,4,5};
+	int sum1=0;
+	int total=15;
+	for(int i=0;i<a.length;i++) {
+		sum1=sum1+a[i];
+	}
+	
+	System.out.println(sum1);
+	System.out.println("The missing number is "+ (total-sum1));
+	}
+	
+}
