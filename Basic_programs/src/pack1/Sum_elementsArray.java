1d29c2037ba52116efcb3e906e50d1a9a125696f
User <User@hp>
2025-10-05 10:35:42 +0530

    Ths sum of the element in array

---
+++ Basic_programs/src/pack1/Sum_elementsArray.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class Sum_elementsArray {
+
+	public static void main(String args[]) {
+		int a []= {10,20,30,40,50};
+		int sum= 0;
+		
+		for(int i=0;i<a.length;i++) {
+			sum= sum+a[i];
+		}
+		System.out.println("Sum of all the number in array is = " +  sum);
+		
+		
+	}
+	
+}
