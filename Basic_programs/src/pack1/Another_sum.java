2bef941001ead8ced87bc050555b0e5af3ac788a
User <User@hp>
2025-10-05 09:46:36 +0530

    This is the sum of the largest number program

--- Basic_programs/src/pack1/Another_sum.java
+++ Basic_programs/src/pack1/Another_sum.java
@@ -1,5 +1,29 @@
 package pack1;
 
+import java.util.Scanner;
+
 public class Another_sum {
 
-}
+	public static void main(String args[]) {
+	System.out.println("this is the find largest number in the sum");
+
+	Scanner sa = new Scanner(System.in);
+	int a = sa.nextInt();
+	Scanner sb = new Scanner(System.in); 
+	int b= sb.nextInt();
+	Scanner sc= new Scanner(System.in);
+int c= 	sc.nextInt();
+	
+	if(a>b&a>c) {
+		System.out.println("a is the largest number");
+		
+	}
+	if(b>a&b>c) {
+		System.out.println("b is the largest number");
+		
+	}if(c>a&c>b) {
+		System.out.println("c is the largest number");
+	}
+	
+	}
+	}
