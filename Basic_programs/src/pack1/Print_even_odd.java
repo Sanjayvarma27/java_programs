e04d5c89f85f9b0aa0d02ebf95291f48c5d8b446
User <User@hp>
2025-10-05 10:43:30 +0530

    This is the array progrma which need to find the even or odd number

---
+++ Basic_programs/src/pack1/Print_even_odd.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class Print_even_odd {
+
+	public static void main(String[] args) {
+		
+	
+	int a[]= {17};
+	
+	if(a[0]%2==0) {
+		
+		System.out.println("it is the even number");
+	}else {
+		System.out.println("it is the odd number");
+	}
+	
+	}
+}
